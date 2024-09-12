34eb94582e3949e965d16ef5d82dc4c918df1da4
matttbates@SurfaceBook <matttbates@hotmail.com>
2024-09-12 02:14:28 -0600

    added new tiles, updated door

--- src/jvmMain/kotlin/tiles/Tile.kt
+++ src/jvmMain/kotlin/tiles/Tile.kt
@@ -38,6 +38,55 @@ enum class Tile(
         sprite = "tree_top",
         isSolid = true,
         inFrontOf = TileTreeTrunk
+    ),
+    TileBuildingBottomLeft(
+        sprite = "building_bottom_left",
+        isSolid = true
+    ),
+    TileBuildingBottom(
+        sprite = "building_bottom",
+        isSolid = true
+    ),
+    TileBuildingBottomRight(
+        sprite = "building_bottom_right",
+        isSolid = true
+    ),
+    TileFlowers(
+        sprite = "flowers",
+        isSolid = false
+    ),
+    TileBuildingTopLeft(
+        sprite = "building_top_left",
+        isSolid = false,
+        inFrontOf = TileGrass
+    ),
+    TileBuildingTop(
+        sprite = "building_top",
+        isSolid = false,
+        inFrontOf = TileGrass
+    ),
+    TileBuildingTopRight(
+        sprite = "building_top_right",
+        isSolid = false,
+        inFrontOf = TileGrass
+    ),
+    TileBuildingMiddleLeft(
+        sprite = "building_middle_left",
+        isSolid = true,
+        inFrontOf = TileGrass
+    ),
+    TileBuildingMiddle(
+        sprite = "building_middle",
+        isSolid = true
+    ),
+    TileBuildingMiddleRight(
+        sprite = "building_middle_right",
+        isSolid = true,
+        inFrontOf = TileGrass
+    ),
+    TileSand(
+        sprite = "sand",
+        isSolid = false
     );
     companion object {
         fun getById(id: Int): Tile? {
